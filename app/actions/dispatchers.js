d69230c6495c6954a406e8071314e082a74d6686
Atticus White <contact@atticuswhite.com>
2015-11-28 18:51:52 -0500

    action dispatchers

---
+++ app/actions/dispatchers.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import {
+  NUMBER_INPUT,
+  NUMBER_SIGNED_INPUT,
+  DECIMAL_INPUT,
+  OPERATION_INPUT,
+  UNDO,
+  CALCULATE
+} from './types';
+
+export function inputNumber(value) {
+  return {
+    type: NUMBER_INPUT,
+    value
+  };
+}
+
+export function inputSigned() {
+  return {
+    type: NUMBER_SIGNED_INPUT
+  };
+}
+
+export function inputDecimal() {
+  return {
+    type: DECIMAL_INPUT
+  };
+}
+
+export function performOperation(operation) {
+  return {
+    type: OPERATION_INPUT,
+    operation
+  };
+}
+
+export function undo() {
+  return {
+    type: UNDO
+  };
+}
+
+export function calculate() {
+  return {
+    type: CALCULATE
+  };
+}
--- app/actions/types.js
+++ app/actions/types.js
@@ -1,5 +1,8 @@
+'use strict';
+
 export const NUMBER_INPUT = 'NUMBER_INPUT';
 export const NUMBER_SIGNED_INPUT = 'NUMBER_SIGNED_INPUT';
+export const DECIMAL_INPUT = 'DECIMAL_INPUT';
 export const OPERATION_INPUT = 'OPERATION_INPUT';
 export const UNDO = 'UNDO';
 export const CALCULATE = 'CALCULATE';
