cb9b06e17d807e5d6bc32a3263849348ae7aa4c1
Atticus White <contact@atticuswhite.com>
2015-11-28 18:48:32 -0500

    action types

---
+++ app/actions/types.js
@@ -0,0 +1,5 @@
+export const NUMBER_INPUT = 'NUMBER_INPUT';
+export const NUMBER_SIGNED_INPUT = 'NUMBER_SIGNED_INPUT';
+export const OPERATION_INPUT = 'OPERATION_INPUT';
+export const UNDO = 'UNDO';
+export const CALCULATE = 'CALCULATE';
