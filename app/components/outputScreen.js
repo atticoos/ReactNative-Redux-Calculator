a27aa1fdd4aa21a1de4df80943d36fbfc17b8740
Atticus White <contact@atticuswhite.com>
2015-11-28 20:51:22 -0500

    show calculation aggregate preview

--- app/components/outputScreen.js
+++ app/components/outputScreen.js
@@ -4,6 +4,13 @@ import React, {Component, StyleSheet, View, Text} from 'react-native';
 import {OPERATION_ADD, OPERATION_SUBTRACT, OPERATION_DIVIDE, OPERATION_MULTIPLY} from '../actions/types';
 import {aggregateCalculatorHistory} from '../helper';
 
+const OperationSymbols = {
+  [OPERATION_ADD]: '+',
+  [OPERATION_SUBTRACT]: '-',
+  [OPERATION_DIVIDE]: '/',
+  [OPERATION_MULTIPLY]: 'X'
+};
+
 class OutputScreen extends Component {
   getOutput() {
     var {currentInput, history} = this.props.calculations;
@@ -15,9 +22,19 @@ class OutputScreen extends Component {
       return currentInput.join('');
     }
   }
+  renderAggregate() {
+    var {currentInput, history, operation} = this.props.calculations;
+    if (currentInput.length > 0) {
+      let aggregate = aggregateCalculatorHistory(history);
+      return (
+        <Text style={styles.aggregate}>{aggregate} {OperationSymbols[operation]}</Text>
+      );
+    }
+  }
   render() {
     return (
       <View style={[styles.view, this.props.style]}>
+        {this.renderAggregate()}
         <Text style={styles.text}>{this.getOutput()}</Text>
       </View>
     )
@@ -26,11 +43,18 @@ class OutputScreen extends Component {
 
 const styles = StyleSheet.create({
   view: {
-    alignItems: 'flex-end'
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'row'
   },
   text: {
     color: '#fff',
     fontSize: 38
+  },
+  aggregate: {
+    color: '#a2dcf5',
+    fontSize: 38,
+    marginRight: 5
   }
 })
 
