836c1228bb8397c34a1c2f7f54b9ecc59d86c96f
Atticus White <contact@atticuswhite.com>
2015-11-28 20:34:14 -0500

    helper function to aggregate calculator history to derive current value

---
+++ app/helper.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import {OPERATION_ADD, OPERATION_SUBTRACT, OPERATION_DIVIDE, OPERATION_MULTIPLY} from './actions/types';
+
+/**
+ * Aggregates the final value of the calculator by the history of inputs
+ */
+export function aggregateCalculatorHistory (calculations, stepFunction = function(){}) {
+  calculations.slice(1, calculations.length).reduce((aggregate, current) => {
+    stepFunction(aggregate, current.input, current.operation);
+    switch (current.operation) {
+      case OPERATION_ADD:
+        return aggregate + current.input;
+      case OPERATION_SUBTRACT:
+        return aggregate - current.input;
+      case OPERATION_DIVIDE:
+        return aggregate / current.input;
+      case OPERATION_MULTIPLY:
+        return aggregate * current.input;
+      default:
+        return aggregate;
+    }
+  }, calculations[0].input);
+}
