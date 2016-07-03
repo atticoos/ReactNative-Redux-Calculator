643e2094ac0597f9d0f01e39562bbe655fc073c4
Atticus White <contact@atticuswhite.com>
2016-07-03 11:22:14 -0400

    map operations

--- app/helper.js
+++ app/helper.js
@@ -3,6 +3,16 @@
 import {OPERATION_ADD, OPERATION_SUBTRACT, OPERATION_DIVIDE, OPERATION_MULTIPLY} from './actions/types';
 
 /**
+ * Operations map
+ */
+const Operations = {
+  [OPERATION_ADD]: (accumulative, current) => accmulative + current,
+  [OPERATION_SUBTRACT]: (accumulative, current) => accumulative - current,
+  [OPERATION_MULTIPLY]: (accumulative, current) => accumulative * current,
+  [OPERATION_DIVIDE]: (accumulative, current) => accumulative / current
+};
+
+/**
  * Aggregates the final value of the calculator by the history of inputs
  */
 export function aggregateCalculatorHistory (calculations, offset = null, stepFunction = function(){}) {
@@ -14,17 +24,10 @@ export function aggregateCalculatorHistory (calculations, offset = null, stepFun
   }
   return collection.slice(1, calculations.length).reduce((aggregate, current, index) => {
     stepFunction(aggregate, current.input, current.operation, index);
-    switch (current.operation) {
-      case OPERATION_ADD:
-        return aggregate + current.input;
-      case OPERATION_SUBTRACT:
-        return aggregate - current.input;
-      case OPERATION_DIVIDE:
-        return aggregate / current.input;
-      case OPERATION_MULTIPLY:
-        return aggregate * current.input;
-      default:
-        return aggregate;
+    if (Operations[current.operation]) {
+      return Operations[current.operation](aggregate, current);
+    }
+    return aggregate;
     }
   }, collection[0].input);
 }
